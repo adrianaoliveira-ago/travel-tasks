eb5e4a37043a64feda702f084e1198dc6d1011ec
Adriana Gonzaga <adrianagonzaga@Adrianas-iMac.fritz.box>
2022-10-17 17:38:05 +0200

    added a new toast

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -6,6 +6,7 @@ function TaskItem({ label }) {
       {/* {label} */}
       <label className="container">
         {label}
+
         <input type="checkbox" />
         <span className="checkmark"></span>
       </label>
